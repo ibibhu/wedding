0d54a4dcffdb5fccba73fafe0d040e83b4772477
Bibhu Prasad <bibhu.prasad026@gmail.com>
2021-06-17 11:52:53 +0530

    Firebase Config

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,14 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 if (!firebase.apps.length) {
   const config = {
-    apiKey: 'AIzaSyCsF8gOmN11MJOE5bqsJIjOxVDUlHhZWS8',
-    authDomain: 'wedding-714df.firebaseapp.com',
-    databaseURL: 'https://wedding-714df-default-rtdb.firebaseio.com',
-    projectId: 'wedding-714df',
-    storageBucket: 'wedding-714df.appspot.com',
-    messagingSenderId: '539013510539',
-    appId: '1:539013510539:web:4c20dbd645eb23bbe86b8e',
-    measurementId: 'G-2ZZ5CLT5ZW',
+    apiKey: 'AIzaSyB9KlcGPrf8fzbUf-hCWAmFuYSOOOBinRA',
+    authDomain: 'chiku-62b3f.firebaseapp.com',
+    projectId: 'chiku-62b3f',
+    storageBucket: 'chiku-62b3f.appspot.com',
+    messagingSenderId: '798066590372',
+    appId: '1:798066590372:web:076b3e8730644e4463dc31',
+    measurementId: 'G-LJENY5XWF7',
   }
   firebase.initializeApp(config)
 }
