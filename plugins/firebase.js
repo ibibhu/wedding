7c3e946df9ed51ed252f026a09d34d863248e4c6
Bibhu Prasad <bibhu.prasad026@gmail.com>
2021-06-22 11:06:56 +0530

    Enable Analytics

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,9 +1,12 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/analytics'
 if (!firebase.apps.length) {
   const config = {
     apiKey: 'AIzaSyB9KlcGPrf8fzbUf-hCWAmFuYSOOOBinRA',
     authDomain: 'chiku-62b3f.firebaseapp.com',
+    databaseURL:
+      'https://chiku-62b3f-default-rtdb.asia-southeast1.firebasedatabase.app',
     projectId: 'chiku-62b3f',
     storageBucket: 'chiku-62b3f.appspot.com',
     messagingSenderId: '798066590372',
@@ -11,6 +14,7 @@ if (!firebase.apps.length) {
     measurementId: 'G-LJENY5XWF7',
   }
   firebase.initializeApp(config)
+  firebase.analytics()
 }
 const fireDb = firebase.firestore()
 export { fireDb }
