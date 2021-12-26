6fd1b47c9c4479c414c79436a5148553141a6b3a
Abinash Patra <Abinash.Patra@go-mmt.com>
2021-12-26 21:11:35 +0530

    fix: moved style tag outside

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
+<style>
+html {
+  font-family: "Oxygen";
+}
+</style>
 <template>
-  <style>
-  html {
-    font-family: "Oxygen";
-  }
-  </style>
   <div class="overflow-hidden">
     <div class="confetti">
       <div class="confetti-piece">
